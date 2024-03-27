4ce34cade56d5b8eaee40e15e080e56e1610acdb
ericemmendorfer <91583186+ericemmendorfer@users.noreply.github.com>
2024-03-27 13:19:22 -0400

    Update example.py

--- example.py
+++ example.py
@@ -10,7 +10,9 @@ def main():
         print("2. Roll a Dice")
         print("3. Draw Cards")
         print("4. Generate Powerball Combination")
-        print("5. Exit")
+        print ("5. Draw from Bag")
+        print ("6. Probability Simulator")
+        print("7. Exit")
 
         choice = input("Enter your choice (1-5): ")
 
@@ -28,10 +30,20 @@ def main():
         elif choice == '4':
             print("Powerball Combination:", generate_powerball())
         elif choice == '5':
+            itemTypes = input("Enter the types of items in the bag separated by commas: ").split(",")
+            itemNums = [int(x) for x in input("Enter the number of each item in the bag separated by commas: ").split(",")]
+            drawNum = int(input("Enter the number of items to draw from the bag: "))
+            print("Draw from Bag:", draw_from_bag(itemTypes, itemNums, drawNum))
+        elif choice == '6':
+            outcomes = input("Enter the possible outcomes separated by commas: ").split(",")
+            probabilities = [float(x) for x in input("Enter the probabilities of each outcome separated by commas: ").split(",")]
+            num_trials = int(input("Enter the number of trials to simulate: "))
+            print("Probability Simulator:", prob_simulator(outcomes, probabilities, num_trials))
+        elif choice == '7':
             print("Exiting the program. Goodbye!")
             break
         else:
-            print("Invalid choice. Please enter a number from 1 to 5.")
+            print("Invalid choice. Please enter a number from 1 to 7.")
 
 if __name__ == "__main__":
     main()
